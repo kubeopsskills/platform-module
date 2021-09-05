7b726bdc9eeae6be515f426e9bf4c83a345b7f0c
dmakeroam <cpfapp@gmail.com>
2021-09-05 18:07:43 +0700

    fix: Invalid value for "path" parameter for installation step

--- argocd/install.tf
+++ argocd/install.tf
@@ -1,5 +1,5 @@
 resource "kubectl_manifest" "argocd" {
   for_each           = fileset(path.module, "installation/${var.cloud_type}/*.yaml")
-  yaml_body          = file(each.value)
+  yaml_body          = file("${path.module}/${each.value}")
   override_namespace = "argocd"
 }
